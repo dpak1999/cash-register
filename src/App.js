d6c34d28bd4efa9f49f964c47e3726001787ba69
dpak1999 <deepakkumardash313@gmail.com>
2021-12-04 20:05:11 +0530

    initla styling

--- src/App.js
+++ src/App.js
@@ -2,10 +2,14 @@
 
 function App() {
   return (
-    <div className="bg-gradient-to-b from-red-300 to-purple-700 min-h-screen flex">
-      <h1 className="text-white font-bold text-4xl m-auto italic font-serif">
-        This is a react tailwind starter template
+    <div className="bg-gray-200 min-h-screen">
+      <h1 className="text-3xl underline text-center font-mono italic pt-4">
+        Cash register
       </h1>
+      <p className="text-center mt-4 text-lg text-indigo-500 font-mono font-bold">
+        Please enter the bill amount and the cash given by customer <br /> to
+        know the minimum number of notes to return
+      </p>
     </div>
   );
 }
